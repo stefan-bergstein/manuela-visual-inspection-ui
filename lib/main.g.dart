06398d488041fb6d412977376241f1797212490a
Kounex <r.schramowski@googlemail.com>
2023-04-12 10:34:37 +0200

    initial setup

---
+++ lib/main.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$damagedYOLOImagesHash() => r'bae27815b52882d4839889b4ee77d0ac9c52d4c4';
+
+/// See also [DamagedYOLOImages].
+@ProviderFor(DamagedYOLOImages)
+final damagedYOLOImagesProvider =
+    AutoDisposeNotifierProvider<DamagedYOLOImages, List<YOLOImage>>.internal(
+  DamagedYOLOImages.new,
+  name: r'damagedYOLOImagesProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$damagedYOLOImagesHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DamagedYOLOImages = AutoDisposeNotifier<List<YOLOImage>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
